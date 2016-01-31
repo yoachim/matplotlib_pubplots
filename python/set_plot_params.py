15d371c91fd4a351e1b075f1e0298962ecd02f97
Peter Yoachim <yoachim@uw.edu>
2016-01-30 20:17:19 -0800

    fuck

--- python/set_plot_params.py
+++ python/set_plot_params.py
@@ -13,18 +13,6 @@ def mpl_single_column(usetex=False):
     plt.rc('text', usetex=usetex)
     plt.rc('savefig', format='pdf', bbox='tight')
 
-def mpl_span_columns(usetex=False):
-    """
-    Set matplotlib to make pretty plots for publishing figs that
-    span 2-columns.
-    """
-    plt.rcdefaults()
-    plt.rc('font', family='serif')
-    plt.rc('figure', figsize=(7,5.5))
-    plt.rc('xtick', labelsize='x-small')
-    plt.rc('ytick', labelsize='x-small')
-    plt.rc('text', usetex=usetex)
-
 def mpl_slides(usetex=False):
     """
     Set matplotlibrc to make pretty slides
@@ -36,9 +24,17 @@ def mpl_slides(usetex=False):
     plt.rc('ytick', labelsize='x-small')
     plt.rc('text', usetex=usetex)
     plt.rc('lines', linewidth=5)
+    plt.rc('savefig', format='pdf', bbox='tight')
 
 def mpl_thumbnails(usetex=False):
     """
     Make png thumbnails
     """
-    # XXX -- change the defaults above to pdf, remove .pdf from filename
+    plt.rcdefaults()
+    plt.rc('font', family='serif')
+    plt.rc('figure', figsize=(4,3))
+    plt.rc('xtick', labelsize='x-small')
+    plt.rc('ytick', labelsize='x-small')
+    plt.rc('text', usetex=usetex)
+    plt.rc('savefig', format='pdf', bbox='tight')
+    plt.rc('savefig', format='png', bbox='tight')
